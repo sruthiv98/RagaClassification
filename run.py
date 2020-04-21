44ef71c515cf61a43ec9d669f4d33d01da634539
Sruthi <sruthi@sruthis-mbp.lan>
2020-04-21 16:25:14 -0700

    fixing ETL pipeline

---
+++ run.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+import sys
+import json
+import shutil
+
+sys.path.insert(0, 'src') # add library code to path
+from etl import load, clean_data #, compute_pitch (do not know if needed)
+#from model import driver
+
+
+DATA_PARAMS = 'config/01-data.json'
+CLEAN_PARAMS = 'config/02-clean.json'
+MODEL_PARAMS = 'config/03-model.json'
+
+TEST_DATA_PARAMS = 'config/test-01-data.json'
+TEST_CLEAN_PARAMS = 'config/test-02-clean.json'
+
+
+def load_params(fp):
+    with open(fp) as fh:
+        param = json.load(fh)
+
+    return param
+
+
+def main(targets):
+
+    # make the clean target
+    if 'clean' in targets:
+        shutil.rmtree('data/raw', ignore_errors=True)
+        shutil.rmtree('data/cleaned', ignore_errors=True)
+        shutil.rmtree('data/out', ignore_errors=True)
+        shutil.rmtree('data/test', ignore_errors=True)
+
+    # make the data target
+    if 'data' in targets:
+        cfg = load_params(DATA_PARAMS)
+        [_ for _ in load(**cfg)]
+
+        cfg = load_params(CLEAN_PARAMS)
+        [_ for _ in clean_data(**cfg)]
+
+    # make the test target
+    if 'test' in targets:
+        cfg = load_params(TEST_DATA_PARAMS)
+        [_ for _ in load(**cfg)]
+
+        cfg = load_params(TEST_CLEAN_PARAMS)
+        [_ for _ in clean_data(**cfg)]
+
+    if 'model' in targets:
+        #cfg = load_params(MODEL_PARAMS)
+        #driver(**cfg)
+        print('working on it')
+
+    return
+
+
+if __name__ == '__main__':
+    targets = sys.argv[1:]
+    main(targets)
---
+++ src/etl.py
@@ -0,0 +1,129 @@
+import numpy as np
+import librosa, librosa.display
+import matplotlib.pyplot as plt
+%matplotlib inline
+import os, numpy, scipy, matplotlib.pyplot as plt, IPython.display as ipd
+from IPython.display import Audio
+import pandas as pd
+
+
+
+def load(indir=None,outdir=None): 
+    songs = os.listdir(indir)
+    y_array = []
+    sr_array = []
+    song_name = []
+    for i in songs:
+        if '.DS_Store' in i: 
+            continue
+        y, sr = librosa.load(indir+'/'+ i)
+        y_array.append(y)
+        sr_array.append(sr)
+        song_name.append(i)
+    df = pd.DataFrame({'Name': song_name,
+                       'y': y_array,
+                       'sr': sr_array})
+    df.to_pickle(os.path.join(outdir,r'loaded_data.pkl'))
+
+    return df
+
+
+def compute_pitch(chromagram):
+    total_occurences_above_8 = []
+    notes = ['C','C#','D','D#','E','F','F#','G','G#','A','A#','B']
+
+    for i in chromagram: 
+        count = 0
+        for j in i: 
+            if j >=0.7: 
+                count+=1
+        total_occurences_above_8.append(count)
+        
+    df = pd.DataFrame({'occurences above 0.8':total_occurences_above_8,'notes':notes})
+    df = df.sort_values('occurences above 0.8',ascending=False)
+    pitch = df.iloc[0]['notes']
+        
+    return pitch
+
+def get_clean_data(df):
+    dfdict = {'songs': [], 'y': [], 'sr': []}
+    fmin = librosa.midi_to_hz(36)
+    hop_length = 512
+    
+    audio = list(df['Name'])
+    ys = np.asarray(list(df['y']))
+    srs = list(df['sr'])
+    
+    #iterate through input dict
+    for i in range(len(df)):
+        y = ys[i]
+        sr = srs[i]
+        
+        #check pitch of each input
+        chromagram = librosa.feature.chroma_stft(y, sr=sr, hop_length=hop_length)
+        
+        pitch = compute_pitch(chromagram)
+        
+        if pitch == 'C':
+            continue
+        elif pitch == 'C#':
+            newy = librosa.effects.pitch_shift(y, sr, n_steps=-1)
+        elif pitch == 'D':
+            newy = librosa.effects.pitch_shift(y, sr, n_steps=-2)
+        elif pitch == 'D#':
+            newy = librosa.effects.pitch_shift(y, sr, n_steps=-3)
+        elif pitch == 'E':
+            newy = librosa.effects.pitch_shift(y, sr, n_steps=-4)
+        elif pitch == 'F':
+            newy = librosa.effects.pitch_shift(y, sr, n_steps=-5)
+        elif pitch == 'F#':
+            newy = librosa.effects.pitch_shift(y, sr, n_steps=-6)
+        elif pitch == 'G':
+            newy = librosa.effects.pitch_shift(y, sr, n_steps=-7)
+        elif pitch == 'G#':
+            newy = librosa.effects.pitch_shift(y, sr, n_steps=+4)
+        elif pitch == 'A':
+            newy = librosa.effects.pitch_shift(y, sr, n_steps=+3)
+        elif pitch == 'A#':
+            newy = librosa.effects.pitch_shift(y, sr, n_steps=+2)
+        elif pitch == 'B':
+            newy = librosa.effects.pitch_shift(y, sr, n_steps=+1)
+        
+        y = newy  
+        ylist = []
+        first = 0
+        second = 3000000
+        while second < len(y):
+            ylist.append(y[first:second])
+            first = second
+            second = second + 3000000
+            
+        cliptitles = []
+        
+        for j in range(len(ylist)):
+            string = audio[i][:-6]
+            cliptitles.append(string)
+            
+        for k in range(len(cliptitles)):
+            dfdict['songs'].append(cliptitles[k])
+            dfdict['y'].append(ylist[k])
+            #assuming sr does not change?
+            dfdict['sr'].append(sr)
+            
+    newdf = pd.DataFrame(dfdict)
+    
+    return None
+
+
+
+def clean_data(indir = None, outdir = None):
+
+    if outdir and not os.path.exists(outdir):
+        os.makedirs(outdir)
+    df = pd.read_pickle('data/loaded_data.pkl')  
+    cleaned = get_clean_data(df)
+   
+    cleaned.to_pickle(os.path.join(outdir,r'cleaned_data.pkl'))
+    
+    return cleaned
+
